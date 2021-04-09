ec913b2f5cb2b11a2986264628bcc2b89195fc1d
Sergio-DC <sgdc377@gmail.com>
2021-04-09 10:38:58 -0500

    First Steps with future and completableFuture

--- src/main/java/agilethought/internship/container/Main.java
+++ src/main/java/agilethought/internship/container/Main.java
@@ -1,5 +1,8 @@
 package agilethought.internship.container;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +11,31 @@ public class Main {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(Main.class, args);
-		System.out.println("Hop");
+//		System.out.println("Hop");
+//		CompletableFuture<String> future = CompletableFuture.completedFuture("TEST");
+//		try {
+//			System.out.println("Immediate future completed: " + future.get());
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (ExecutionException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		CompletableFuture<String> future2 = new CompletableFuture<String>();
+		
+		try {
+//			System.out.println("Is future2 completed?: " + future2.get());
+			future2.complete("finished");
+			System.out.println("Is future2 completed?: " + future2.get());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
